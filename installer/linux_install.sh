6da13e28738ec0045c884b168445875f83d79551
Rodolphe Pineau <pineau@rti-zone.org>
2018-11-23 18:31:42 -0800

    fix linux install script as apparently the new plugin directory will be PlugIns64

--- installer/linux_install.sh
+++ installer/linux_install.sh
@@ -6,9 +6,7 @@ echo "TheSkyX_Install = $TheSkyX_Install"
 if [ ! -f "$TheSkyX_Install" ]; then
     echo TheSkyXInstallPath.txt not found
     TheSkyX_Path=`/usr/bin/find ~/ -maxdepth 3 -name TheSkyX`
-    if [ -d "$TheSkyX_Path" ]; then
-		TheSkyX_Path="${TheSkyX_Path}/Contents"
-    else
+    if [ ! -d "$TheSkyX_Path" ]; then
 	   echo TheSkyX application was not found.
     	exit 1
 	 fi
@@ -24,14 +22,20 @@ if [ ! -d "$TheSkyX_Path" ]; then
     exit 1
 fi
 
+if [ -d "$TheSkyX_Path/Resources/Common/PlugIns64" ]; then
+	PLUGINS_DIR="PlugIns64"
+else
+	PLUGINS_DIR="PlugIns"
+fi
+
 cp "./focuserlist ArduinoFocus.txt" "$TheSkyX_Path/Resources/Common/Miscellaneous Files/"
-cp "./ArduinoFocus.ui" "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/"
-cp "./libArduinoFocus.so" "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/"
+cp "./ArduinoFocus.ui" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/"
+cp "./libArduinoFocus.so" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/"
 
 app_owner=`/usr/bin/stat -c "%u" "$TheSkyX_Path" | xargs id -n -u`
 if [ ! -z "$app_owner" ]; then
 	chown $app_owner "$TheSkyX_Path/Resources/Common/Miscellaneous Files/focuserlist ArduinoFocus.txt"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/ArduinoFocus.ui"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/libArduinoFocus.so"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/ArduinoFocus.ui"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/libArduinoFocus.so"
 fi
-chmod  755 "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/libArduinoFocus.so"
+chmod  755 "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/libArduinoFocus.so"
