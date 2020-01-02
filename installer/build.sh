72d3fc85c2b3ebd1c759a9735cd7a4715f3a0dea
Rodolphe Pineau <pineau@rti-zone.org>
2020-01-02 08:54:35 -0800

    Fix for 64 bit macOS code signing

--- installer/build.sh
+++ installer/build.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+PACKAGE_NAME="ArduinoFocus_X2.pkg"
+BUNDLE_NAME="org.rti-zone.ArduinoFocusX2"
+
+if [ ! -z "$app_id_signature" ]; then
+    codesign -s "$app_id_signature" ../build/Release/libArduinoFocus.dylib
+fi
+
 mkdir -p ROOT/tmp/ArduinoFocus_X2/
 cp "../ArduinoFocus.ui" ROOT/tmp/ArduinoFocus_X2/
 cp "../focuserlist ArduinoFocus.txt" ROOT/tmp/ArduinoFocus_X2/
 cp "../build/Release/libArduinoFocus.dylib" ROOT/tmp/ArduinoFocus_X2/
 
+
 if [ ! -z "$installer_signature" ]; then
-# signed package using env variable installer_signature
-pkgbuild --root ROOT --identifier org.rti-zone.ArduinoFocus_X2 --sign "$installer_signature" --scripts Scripts --version 1.0 ArduinoFocus_X2.pkg
-pkgutil --check-signature ./ArduinoFocus_X2.pkg
+	# signed package using env variable installer_signature
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --sign "$installer_signature" --scripts Scripts --version 1.0 $PACKAGE_NAME
+	pkgutil --check-signature ./${PACKAGE_NAME}
+
 else
-pkgbuild --root ROOT --identifier org.rti-zone.ArduinoFocus_X2 --scripts Scripts --version 1.0 ArduinoFocus_X2.pkg
+    pkgbuild --root ROOT --identifier $BUNDLE_NAME --scripts Scripts --version 1.0 $PACKAGE_NAME
 fi
 
 rm -rf ROOT
